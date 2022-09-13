71eb2874717433e818b0b6eb7501814981694962
AquaApple <yassershehab9@gmail.com>
2022-09-13 22:39:40 +0200

    Used the link component to navigate between pages

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,17 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 //ourDoman-name/news/somethingImportant
 
 function DetailPage() {
+  const router = useRouter();
+
+  console.log(router.query.newsId);
+
+  useEffect(() => {
+    console.log("Use Effect Running boy");
+  }, []);
+
   return <h1>The Detail Page</h1>;
 }
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextJs-is-a-great-framework">NextJs Is A Great Framework</Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
